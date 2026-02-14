b4e135482fb4810f1a1fd2e77733f58cc59eeb59
Aandreba <aandreba@gmail.com>
2026-02-14 13:03:57 +0100

    vv

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -2048,7 +2048,6 @@ _Static_assert(offsetof(ArchCPU, env) + offsetof(CPUX86State, xmm_regs[5]) == 0x
 _Static_assert(offsetof(ArchCPU, env) + offsetof(CPUX86State, xmm_regs[6]) == 0x2c90, "");
 _Static_assert(offsetof(ArchCPU, env) + offsetof(CPUX86State, xmm_regs[7]) == 0x2cd0, "");
 #else
-_Static_assert(false, "Hii!!")
 _Static_assert(offsetof(ArchCPU, env) + offsetof(CPUX86State, fpregs[0]) == 0x2960, "");
 #endif
 
